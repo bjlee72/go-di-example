ed1cb1206cbf370787223ca86a33bb20111acf87
Byungjoon Lee <byungjoon.lee@gmail.com>
2024-01-07 11:41:35 -0800

    Make a main.go a simple HTTP server

--- cmd/di/main.go
+++ cmd/di/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/bjlee72/go-di-example/cmd/di/internal/adaptor"
 	"github.com/bjlee72/go-di-example/component"
@@ -12,5 +14,12 @@ func main() {
 	cmp := component.New(
 		adaptor.ToDataProvider(
 			db.New()))
+
 	fmt.Println(cmp.GetAll())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, fmt.Sprintf("Hello: %v", cmp.GetAll()))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
